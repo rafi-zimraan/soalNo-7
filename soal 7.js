d3c38c12c41d118a50abba27799a631605cba2fa
rafi-zimraan <raffizimraan27@gmail.com>
2023-02-16 19:16:23 +0700

    first commit

--- soal 7.js
+++ soal 7.js
@@ -13,14 +13,3 @@ for (let v = 0; v < 17; v+=2) {
 
 
 
-// for (let i = 0; i < 17; i+=2){
-//     for (let j =0; j < 17; j+=2){
-//         if(i == 8 || i + j == 16){
-//             document.write(j + " ");
-//         } else if(j == 8 || j == i){
-//             document.write((i+1) + " ");
-//         } else {
-//             document.write(" &nbsp ");
-//         }
-//     }document.write("<br>")
-// }
